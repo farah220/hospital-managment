d2ed98ea4286d33571355fb6dfa79feab0a779bd
Esraa Ehab <esraaaehabb@gmail.com>
2023-04-02 03:56:49 +0200

    pharmacist

--- database/migrations/2023_03_22_151243_create_pharmacists_table.php
+++ database/migrations/2023_03_22_151243_create_pharmacists_table.php
@@ -17,6 +17,8 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email');
+            $table->string('phone');
+            $table->string('password');
             $table->string('image')->nullable();
             $table->timestamps();
         });
