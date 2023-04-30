e011b3bd2db195049da75b1d2d20b05255982eeb
farah220 <68689314+farah220@users.noreply.github.com>
2023-04-30 22:12:38 +0300

    Update pharmacist-dashboard.php

--- routes/pharmacist-dashboard.php
+++ routes/pharmacist-dashboard.php
@@ -6,9 +6,5 @@ use Illuminate\Support\Facades\Route;
 Route::group(['namespace' => 'PharmacistDashboard', 'as' => 'dashboard.', 'middleware' => ['web','auth:pharmacists']],function (){
 Route::view('/','pharmacist-dashboard.index');
 Route::resource('medicines','MedicineController');
-
-//Route::resource('admins','AdminController');
 Route::post('/logout','MedicineController@logOut')->name('pharmacist.logout');
-
-
 });
